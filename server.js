db99dd8b877838ef07c6b1ec00618c4c0ba174e1
Matheu Plouffe <mp@MacBook-Pro.local>
2018-07-13 18:11:29 -0500

    Servering up a basic website with hbs view engine.

--- server.js
+++ server.js
@@ -1,22 +1,52 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('currentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('shout', (text) => {
+    return text.toUpperCase();
+});
 
-app.get('/', (req, res) => {
-    res.send({
-        name: 'Matheu',
-        likes: [
-            'Node.js',
-            'World Domination!'
-        ]
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server.log.');
+        }
+    });
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maitenance.hbs', {
+        pageTitle: 'UNDER CONSTRUCTION',
+        errorMessage: "We're working on it..."
     });
 });
 
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome... WELCOME TO YOUR DOOM!!!!! MUHAHAHAHAHA!!!!!'
+    })
+});
+
 app.get('/about', (req, res) => {
-    res.send('About Page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
 });
 
 app.get('/bad', (req, res) => {
